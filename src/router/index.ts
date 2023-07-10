1274d44382335bdc2fca905cd11aa263fbca1b2e
田志会 Chris <chris.tian@biposervice.com>
2023-07-10 18:03:18 +0800

    first commit

--- src/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory } from "vue-router/auto";
-import { setupLayouts } from "../utils/seupLayouts";
-
-const router = createRouter({
-  history: createWebHistory(),
-  extendRoutes: setupLayouts,
-});
-
-export default router;
